cf4e0c3506101ab34487ba0dc73579f93b1c1e11
Harshavardhan <187y1a0575@gmail.com>
2020-08-05 22:48:56 +0530

    SECOND COMMIT

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,19 @@
+import React,{createContext,Component} from 'react';//Extending createContext library to create a context
+export const ThemeContext=createContext();//Creating a context named ThemeContext
+
+class ThemeContextProvider extends Component {
+    state = {                                       //Creating state which will be exported to be imported in another component(This is a named export)
+        isLightTheme:true,
+        light:{syntax:"#555",ui:"#ddd",bg:"#eee"},
+        dark:{syntax:"#ddd",ui:"#333",bg:"#555"}
+    }
+    render() { 
+        return (
+            <ThemeContext.Provider value={{...this.state}}>
+                {this.props.children}{/*Whatever is enclosed in b/w the contextProvider component will use this state*/}
+            </ThemeContext.Provider>
+        );
+    }
+}
+ 
+export default ThemeContextProvider;//exporting the provider
